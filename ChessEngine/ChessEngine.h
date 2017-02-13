9a4bd4932cfb35dc293c52e5d0859cacbaac7092
hengyu <hengyu@outlook.com>
2017-02-13 16:16:19 +0800

    Initial Commit

---
+++ ChessEngine/ChessEngine.h
@@ -0,0 +1,19 @@
+//
+//  ChessEngine.h
+//  ChessEngine
+//
+//  Created by hengyu on 2017/2/13.
+//  Copyright © 2017年 hengyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChessEngine.
+FOUNDATION_EXPORT double ChessEngineVersionNumber;
+
+//! Project version string for ChessEngine.
+FOUNDATION_EXPORT const unsigned char ChessEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChessEngine/PublicHeader.h>
+
+
